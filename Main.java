b1b6b4e1c55257cf2f0837dff85e7fc6b3da2579
Donthula Nithish <98210183+DNG0101@users.noreply.github.com>
2026-02-23 17:31:39 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("JAVA PROGRAM EXECUTED FROM WEB BUTTON!");
+        System.out.println("SUCCESS: Java executed from website button!");
     }
 }
