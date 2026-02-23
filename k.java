4fab1474d83a6d4facd3f9e67e62f5bdce4d0ddf
Donthula Nithish <98210183+DNG0101@users.noreply.github.com>
2026-02-23 18:43:31 +0530

    Update k.java

--- k.java
+++ k.java
@@ -1,5 +1,5 @@
 public class k {
-    public static void main(String[] args){
-        System.out.println("Hello from K");
+    public static void main(String[] args) {
+        System.out.println("k.java executed successfully!");
     }
 }
